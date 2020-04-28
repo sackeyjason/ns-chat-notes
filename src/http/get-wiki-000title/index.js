c4acd44b3e67bb22beba60c35fa106fdc65f110f
Jason Sackey <jason.sackey@novastonemedia.com>
2020-04-28 21:40:22 +0100

    posting, reading

--- src/http/get-wiki-000title/index.js
+++ src/http/get-wiki-000title/index.js
@@ -4,13 +4,16 @@ const data = require("@begin/data");
 exports.handler = async function http(req) {
   // let request = arc.http.helpers.bodyParser(req);
   let page = await data.get({ table: "wiki", key: req.pathParameters.title });
+  if (!page) {
+    page = { content: [] };
+  }
   return {
     headers: {
       "cache-control":
         "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
       "content-type": "text/html; charset=utf8",
+      "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify(page),
-    cors: true
   };
 };
--- src/http/post-wiki/index.js
+++ src/http/post-wiki/index.js
@@ -1,19 +1,22 @@
 let data = require("@begin/data");
-let arc = require('@architect/functions');
+let arc = require("@architect/functions");
 
 exports.handler = async function http(req) {
-  let message = arc.http.helpers.bodyParser(req);
+  // let message = arc.http.helpers.bodyParser(req);
+  console.log(req);
+  let body = arc.http.helpers.bodyParser(req);
   await data.set({
     table: "wiki",
-    key: message.title,
-    content: message.page,
+    key: body.title,
+    content: body.page,
   });
   return {
     headers: {
       "cache-control":
         "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
       "content-type": "text/html; charset=utf8",
+      "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify(message)
+    body: "ok",
   };
 };
