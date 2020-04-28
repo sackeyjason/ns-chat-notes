ddb568199fc76b5769e90f5b8d0bc033b03b943c
Jason Sackey <jason.sackey@novastonemedia.com>
2020-04-28 22:18:36 +0100

    workaround bodyParser bug

--- src/http/post-wiki/index.js
+++ src/http/post-wiki/index.js
@@ -5,6 +5,8 @@ exports.handler = async function http(req) {
   // let message = arc.http.helpers.bodyParser(req);
   console.log(req);
   let body = arc.http.helpers.bodyParser(req);
+  body = JSON.parse(Buffer.from(body, 'base64').toString('ascii'));
+  console.log('body: ', body);
   await data.set({
     table: "wiki",
     key: body.title,
@@ -15,7 +17,8 @@ exports.handler = async function http(req) {
       "cache-control":
         "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
       "content-type": "text/html; charset=utf8",
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST",
     },
     body: "ok",
   };
